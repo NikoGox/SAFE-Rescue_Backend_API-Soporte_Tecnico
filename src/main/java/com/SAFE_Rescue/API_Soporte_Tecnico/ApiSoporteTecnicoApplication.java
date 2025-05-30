9b513c59960f142314abd1a3e6cd645f48800b2c
Rubén Parada Peña <nico.94lta@gmail.com>
2025-05-30 06:06:11 -0400

    Inicializacion del repositorio para la API de Servicio Técnico

---
+++ src/main/java/com/SAFE_Rescue/API_Soporte_Tecnico/ApiSoporteTecnicoApplication.java
@@ -0,0 +1,13 @@
+package com.SAFE_Rescue.API_Soporte_Tecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSoporteTecnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSoporteTecnicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SAFE_Rescue/API_Soporte_Tecnico/ApiSoporteTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SAFE_Rescue.API_Soporte_Tecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSoporteTecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
